e827803b8f27234810ccccce45a3dc3f94f3c782
DarkCodeOrg <davidclapton@protonmail.com>
2020-08-03 15:33:06 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # Text-Editor
 this is a small text-editor in C
-
-the site used for reference and learning is  :: https://viewsourcecode.org/snaptoken/kilo/index.html
-
--- main.c
+++ main.c
@@ -5,10 +5,26 @@
 #include<ctype.h>
 #include<termios.h>
 
+struct termios orig_termios; // the original terminal attributes are stored in this variable orig_termios
+void disableRawmode()
+{
+    tcsetattr(STDIN_FILENO,TCSAFLUSH,&orig_termios);
+}
+
+void enableRawmode()
+{
+    tcgetattr(STDIN_FILENO, &orig_termios); // The tcgetattr() function shall get the parameters associated with the terminal referred to by fildes and store them in the termios structure referenced by raw (here). The fildes argument is an open file descriptor associated with a terminal
+    atexit(disableRawmode);
+    struct termios raw = orig_termios ;
+    raw.c_cflag &= ~(ECHO);
+    tcsetattr(STDIN_FILENO,TCSAFLUSH,&raw);
+
+}
 
 
 int main()
 {
+    enableRawmode();
     char c;
     while (read(STDIN_FILENO,&c,1) == 1)   // Read is called to read 1 byte from stdin until there are no more bytes to read i.e the result becomes Zero
     {
